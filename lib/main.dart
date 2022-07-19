69f0acdbda53763f273ac5c44dfd1a2c20afc1cf
GemperC <tom.cohen99@gmail.com>
2022-07-19 18:36:48 +0300

    change contats and premission package to flutter_contacts

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:contacts_service/contacts_service.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MainPage());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Adashim',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Adashim'),
-    );
-  }
+  _MainPageState createState() => _MainPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Contact> contacts = [];
+class _MainPageState extends State<MainPage> {
+  List<Contact>? _contacts;
+  bool _permissionDenied = false;
 
   @override
   void initState() {
     super.initState();
-    checkPermission_contacts();
-    getAllContacts();
-
+    _fetchContacts();
   }
 
-  getAllContacts() async {
-    List<Contact> _contacts = await ContactsService.getContacts();
-    setState(() {
-      contacts = _contacts;
-    });
+  Future _fetchContacts() async {
+    if (!await FlutterContacts.requestPermission(readonly: true)) {
+      setState(() => _permissionDenied = true);
+    } else {
+      final contacts = await FlutterContacts.getContacts();
+      setState(() => _contacts = contacts);
+    }
   }
-
-  checkPermission_contacts() async {
-    var contactStatus = await Permission.contacts.status;
-
-    if (!contactStatus.isGranted){
-      await Permission.contacts.request();}
-  }
-
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('Adashim')),
+          body: _body()));
 
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Text("My Contacts"),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: contacts.length,
-              itemBuilder: (context, index){
-                Contact contact = contacts[index];
-                return ListTile(
-                  title: Text(contact.displayName.toString()),
-                  subtitle: Text(
-                      (contact.phones?.elementAt(0).value).toString()
-                  ),
-                );
-              },
-            )
-          ],
-        ),
-      )
-    );
+  Widget _body() {
+    if (_permissionDenied) return Center(child: Text('Permission denied'));
+    if (_contacts == null) return Center(child: CircularProgressIndicator());
+    return ListView.builder(
+        itemCount: _contacts!.length,
+        itemBuilder: (context, i) => ListTile(
+            title: Text(_contacts![i].displayName),
+            onTap: () async {
+              final fullContact =
+              await FlutterContacts.getContact(_contacts![i].id);
+              await Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => ContactPage(fullContact!)));
+            }));
   }
 }
 
+class ContactPage extends StatelessWidget {
+  final Contact contact;
+  ContactPage(this.contact);
 
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: Text(contact.displayName)),
+      body: Column(children: [
+        Text('First name: ${contact.name.first}'),
+        Text('Last name: ${contact.name.last}'),
+        Text(
+            'Phone number: ${contact.phones.isNotEmpty ? contact.phones.first.number : '(none)'}'),
+        Text(
+            'Email address: ${contact.emails.isNotEmpty ? contact.emails.first.address : '(none)'}'),
+      ]));
+}
